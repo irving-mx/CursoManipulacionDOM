a4742d3652d78829137edc9069d361d623037d3d
Irving mtz <al2173074687@azc.uam.mx>
2024-08-25 20:05:28 -0600

    Proyecto: añadir tareas desde un formulario

--- Project/addTasks/app.js
+++ Project/addTasks/app.js
@@ -5,8 +5,41 @@ const taskList = document.getElementById("task-list");
 taskForm.addEventListener("submit",(event)=>{
     event.preventDefault();
 
-    const taskInput = document.getElementById("task-input");
+const taskInput = document.getElementById("task-input");
 
-    const task = taskInput.value;
-    console.log(task)
-})
+const task = taskInput.value;
+console.log(task)
+
+if(task){
+    taskList.append(createTaskElement(task));
+    taskInput.value=" ";
+}
+});
+
+function createTaskElement(task){
+    const li = document.createElement("li");
+    li.textContent=task;
+    li.append(createButton("❌","delete-btn"),createButton("✏️","edit-btn"));
+    return li;
+}
+
+function createButton(text,className){
+    const btn = document.createElement("span");
+    btn.textContent = text;
+    btn.className = className;
+    return btn;
+}
+
+//     if(task){
+//         const newLi = document.createElement("li")
+//         newLi.innerHTML= `
+//                     ${task}
+//                     <span class="delete-btn">❌</span>
+//                     <span class="edit-btn">✏️</span>
+//         `
+//         taskList.appendChild(newLi)
+//         console.log("nodo agregado")
+//     }else{
+//         console.log("cadena vacia")
+//     }
+// })
